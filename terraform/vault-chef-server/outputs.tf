947579208e0cefcdd66bd7b62ecc86945eeaa179
tdsacilowski <tdsacilowski@gmail.com>
2018-01-16 13:15:03 -0500

    Updated output

--- terraform/vault-chef-server/outputs.tf
+++ terraform/vault-chef-server/outputs.tf
@@ -1,3 +1,3 @@
 output "vault-connection" {
-  value = "${format("ssh -i %s %s@%s", var.key_name, "ubuntu", module.vault_instance.vault-public-ip)}"
+  value = "${format("ssh -i %s.pem %s@%s", var.key_name, "ubuntu", module.vault_instance.vault-public-ip)}"
 }
